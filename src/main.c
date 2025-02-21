8e906e0604d9f9999fa5fae0fb9645e27bf66454
Dardo95 <dsanto-tomas@hotmail.com>
2025-02-21 01:05:47 +0100

    Choose fractol, not julia implementation 2025-02-21 01:05:47

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ int init_data(t_data *data)
 	data->maxRe = 1.0; 
 	data->minIm = -1.2;
 	data->maxIm = 1.2;
-	data->maxIter = 50;
+	data->maxIter = 60;
 
 	data->mlx = mlx_init(WIDTH, HEIGHT, "fractol", true);
 	if (!data->mlx)
@@ -44,11 +44,34 @@ void cleanup(t_data *data)
 		mlx_terminate(data->mlx);
 }
 
-int	main()
+static int choose_fractal(char **argv, t_data *data)
+{
+    if (ft_strncmp(argv[1], "mandelbrot", 10) == 0)
+        draw_mandelbrot(data);
+    else if (ft_strncmp(argv[1], "julia", 5) == 0)
+    {
+        ft_printf("hola\n");
+        return (EXIT_SUCCESS);
+    }
+    else
+    {
+        ft_printf("Unrecognized fractal. Use 'mandelbrot' or 'julia'.\n");
+        return (EXIT_FAILURE);
+    }
+	return (EXIT_SUCCESS);
+}
+
+
+int	main(int argc, char **argv)
 {
 
 	t_data data;
 
+	if (argc < 2)
+    {
+        ft_printf("Usage: ./fractol mandelbrot | julia [cRe cIm]\n");
+        return (EXIT_FAILURE);
+    }
 	if (!init_data(&data))
 		return (EXIT_FAILURE);
 	if (mlx_image_to_window(data.mlx, data.img, 0, 0))
@@ -57,7 +80,7 @@ int	main()
 		cleanup(&data);
 		return (EXIT_FAILURE);
 	}
-	draw_mandelbrot(&data);
+	choose_fractal(argv, &data);
 	mlx_scroll_hook(data.mlx, handle_scroll, &data);
 	mlx_key_hook(data.mlx, handle_keys, &data);
 	mlx_loop(data.mlx);
